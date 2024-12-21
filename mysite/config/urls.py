d5156e3e622577755d505f44110f5100518305be
Kwang Myung Yu <sguys99@gmail.com>
2024-12-21 17:06:12 +0900

    :sparkles: Complete ch02-01
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from pybo import views  # 더 이상 필요하지 않으므로 삭제
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path('pybo/', views.index),
+    path('pybo/', include('pybo.urls')), 
 ]
